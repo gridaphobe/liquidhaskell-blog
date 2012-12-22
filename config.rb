9a90b7e5839c472acd339a35e28f1e51e87e9566
Ranjit Jhala <jhala@cs.ucsd.edu>
2012-12-21 16:26:22 -0800

    changing favicon

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/~/public_html/liquidhaskell/blog/"
+http_images_path = "/~/public_html/liquidhaskell/blog/images"
+http_fonts_path = "/~/public_html/liquidhaskell/blog/fonts"
+css_dir = "public/~/public_html/liquidhaskell/blog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
